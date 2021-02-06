b220fbd7ad3f70a6cbb9429f2d3354f3c4d5e257
Aizhan <190103353@stu.sdu.edu.kz>
2021-02-06 16:08:32 +0600

    creat test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php
+
+$x = 5985;
+var_dump(is_int($x));
+
+echo "<br>";
+
+$x = 59.85;
+var_dump(is_int($x));
+?>  
+	
+</body>
+</html>
